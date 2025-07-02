947e89cae6ebb0d91d61d9d7fa27eac597e56a69
denial-E <shempeter221992@gmail.com>
2025-07-03 01:51:32 +0530

    run bro

---
+++ Models/employee.schema.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const empSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  designation: String,
+});
+const Employee = mongoose.model("Employee", empSchema);
+export default Employee;
