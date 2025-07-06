0f4670482a8bba82058acc0b85d4272395e89ceb
denial-E <shempeter221992@gmail.com>
2025-07-07 01:23:09 +0530

    run bro

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import express from 'express';
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./Database/config.js"
-import empRouter from "./Routers/employee.router.js"
+import connectDB from "./Database/config.js";
+import empRouter from "./Routers/employee.router.js";
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-const port = process.env.port;
+const port = process.env.PORT;
 
 connectDB();
 app.use("/api", empRouter);
 app.listen(port, () => {
-  console.log("App  is running on the port-",port);
+  console.log("App  is running on the port-", port);
 });
